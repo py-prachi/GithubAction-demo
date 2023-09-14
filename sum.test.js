dc77a6edf2a7a1c61a99ef075a086d154f2547e7
py-prachi <prachi.249@gmail.com>
2023-09-14 19:47:21 +0530

    failing test

--- sum.test.js
+++ sum.test.js
@@ -4,5 +4,10 @@ const add = require('./sum')
 
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
-  //expect(1 + 2).toBe(3);
+  
 });
+
+test('adds 1 + 1 to equal 3', () => {
+    expect(add(1, 1)).toBe(3);
+    
+  });
