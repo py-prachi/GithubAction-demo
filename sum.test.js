d2811337e496ccf2ab7b706fb60c63f8b3130afc
py-prachi <prachi.249@gmail.com>
2023-09-14 19:56:29 +0530

    fixed test

--- sum.test.js
+++ sum.test.js
@@ -7,7 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
   
 });
 
-test('adds 2 + 2 to equal 3', () => {
-    expect(add(2, 2)).toBe(3);
+test('adds 2 + 2 to equal 4', () => {
+    expect(add(2, 2)).toBe(4);
     
   });
