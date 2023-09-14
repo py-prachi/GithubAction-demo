1caa192ad2b614f224fb0a9531fe734c6ba1d629
py-prachi <prachi.249@gmail.com>
2023-09-14 20:02:48 +0530

    retest fail

--- sum.test.js
+++ sum.test.js
@@ -7,7 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
   
 });
 
-test('adds 2 + 2 to equal 4', () => {
-    expect(add(2, 2)).toBe(4);
+test('adds 2 + 4 to equal 4', () => {
+    expect(add(2, 4)).toBe(4);
     
   });
